851070fba1f8c037d5e01f298cd5af14fcd064e8
Rob Sanheim <rsanheim@gmail.com>
2020-09-03 13:50:04 -0500

    more things

--- spec/requests/albums_request_spec.rb
+++ spec/requests/albums_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Albums", type: :request do
-
+  it "can favorite an album" do
+    user = User.create! email: "test@example.com", password: "password"
+    sign_in user
+    album = Album.create!(title: "test", artist: "anyone")
+    post favorite_album_path(album)
+    expect(user.favorites.first.favoritable).to eq(album)
+  end
 end
