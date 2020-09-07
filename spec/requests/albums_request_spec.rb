558f8533f3b308f61194d24a355a77f8834eb956
Rob Sanheim <rsanheim@gmail.com>
2020-09-07 13:43:05 -0500

    add spec

--- spec/requests/albums_request_spec.rb
+++ spec/requests/albums_request_spec.rb
@@ -1,11 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "Albums", type: :request do
+  it "can create" do
+    user = User.create! email: "test@example.com", password: "password"
+    sign_in user
+    expect {
+      post albums_path, params: { album: { artist: "artist", title: "test"}}
+      expect(response).to have_http_status(302)
+    }.to change {Album.count}.by(1)
+  end
+
   it "can favorite an album" do
     user = User.create! email: "test@example.com", password: "password"
     sign_in user
     album = Album.create!(title: "test", artist: "anyone")
-    post favorite_album_path(album)
+    expect {
+      post favorite_album_path(album)
+    }.to change {user.favorites.count}.by(1)
     expect(user.favorites.first.favoritable).to eq(album)
   end
 end
